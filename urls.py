70218f50b99fffc6ce18934fe0000b42ea2f4db9
dbailey7 <david.bailey@technologist.com>
2019-06-14 03:00:46 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,28 +1,14 @@
-"""cmpbltrproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include;
-from django.contrib import admin;
+from django.conf.urls import url;
 from cmpbltrapp import views;
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    # url(r'^(?P<pk>[-\w]+)/$', views.mp_archetypeDetailView.as_view(), name='adetail'),
-    # url(r'^mp_archetype_form/', views.mp_archetype_form_view, name='mp_archetype_form'),
-    # url(r'^mp_element_form/', views.mp_element_form_view, name='mp_element_form'),
-    url(r'^cmpbltrapp/', include('cmpbltrapp.urls', namespace='cmpbltrapp')),  # create mapping of app to url
+app_name = 'cmpbltrapp';  # Need this so we can ref, say, cmpbltrapp:list, etc in the templates
 
+urlpatterns = [
+    url(r'^$', views.index, name='index'), # Need name for same reason in templates
+    url(r'^archetype_list/', views.mp_archetypeListView.as_view(), name='alist'), # Need name for same reason in templates
+    url(r'^(?P<pk>\d+)/$', views.mp_archetypeDetailView.as_view(model='mp_archetype', template_name='cmpbltrapp/mp_archetype_detail.html'), name='mp_archetype-detail'),
+    url(r'^mp_archetype_form/', views.mp_archetype_form_view, name='aform'), # Need name for same reason in templates
+    url(r'^element_list/', views.mp_elementListView.as_view(), name='elist'), # Need name for same reason in templates
+    url(r'^(?P<pk>\d+)/$', views.mp_elementDetailView.as_view(), name='edetail'), # Need name for same reason in templates
+    url(r'^mp_element_form/', views.mp_element_form_view, name='eform'), # Need name for same reason in templates
 ];
